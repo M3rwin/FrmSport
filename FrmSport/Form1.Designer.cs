0e9c9e4173825a05740e594ae348d4db7c9f19c2
Merwyn Ra <marcoetude@gmail.com>
2023-04-12 11:31:12 +0200

    j'ai envie de me pendre

--- FrmSport/Form1.Designer.cs
+++ FrmSport/Form1.Designer.cs
@@ -31,17 +31,18 @@ namespace FrmSport
         {
             this.GrpSports = new System.Windows.Forms.GroupBox();
             this.GrpSaisieSport = new System.Windows.Forms.GroupBox();
-            this.TxtSport = new System.Windows.Forms.TextBox();
             this.BtnAjout = new System.Windows.Forms.Button();
+            this.TxtSport = new System.Windows.Forms.TextBox();
             this.BtnQuitter = new System.Windows.Forms.Button();
             this.GrpSaisieSport.SuspendLayout();
             this.SuspendLayout();
             // 
             // GrpSports
             // 
-            this.GrpSports.Location = new System.Drawing.Point(291, 151);
+            this.GrpSports.AutoSize = true;
+            this.GrpSports.Location = new System.Drawing.Point(235, 12);
             this.GrpSports.Name = "GrpSports";
-            this.GrpSports.Size = new System.Drawing.Size(200, 65);
+            this.GrpSports.Size = new System.Drawing.Size(221, 79);
             this.GrpSports.TabIndex = 0;
             this.GrpSports.TabStop = false;
             this.GrpSports.Text = "Sports";
@@ -51,12 +52,23 @@ namespace FrmSport
             // 
             this.GrpSaisieSport.Controls.Add(this.BtnAjout);
             this.GrpSaisieSport.Controls.Add(this.TxtSport);
-            this.GrpSaisieSport.Location = new System.Drawing.Point(291, 222);
+            this.GrpSaisieSport.Location = new System.Drawing.Point(12, 12);
             this.GrpSaisieSport.Name = "GrpSaisieSport";
             this.GrpSaisieSport.Size = new System.Drawing.Size(200, 52);
             this.GrpSaisieSport.TabIndex = 1;
             this.GrpSaisieSport.TabStop = false;
             this.GrpSaisieSport.Text = "Saisie d\'un sport";
+            // 
+            // BtnAjout
+            // 
+            this.BtnAjout.Enabled = false;
+            this.BtnAjout.Location = new System.Drawing.Point(112, 21);
+            this.BtnAjout.Name = "BtnAjout";
+            this.BtnAjout.Size = new System.Drawing.Size(75, 23);
+            this.BtnAjout.TabIndex = 1;
+            this.BtnAjout.Text = "Ajouter";
+            this.BtnAjout.UseVisualStyleBackColor = true;
+            this.BtnAjout.Click += new System.EventHandler(this.BtnAjout_Click);
             // 
             // TxtSport
             // 
@@ -64,24 +76,17 @@ namespace FrmSport
             this.TxtSport.Name = "TxtSport";
             this.TxtSport.Size = new System.Drawing.Size(100, 23);
             this.TxtSport.TabIndex = 0;
-            // 
-            // BtnAjout
-            // 
-            this.BtnAjout.Location = new System.Drawing.Point(112, 21);
-            this.BtnAjout.Name = "BtnAjout";
-            this.BtnAjout.Size = new System.Drawing.Size(75, 23);
-            this.BtnAjout.TabIndex = 1;
-            this.BtnAjout.Text = "Ajouter";
-            this.BtnAjout.UseVisualStyleBackColor = true;
+            this.TxtSport.TextChanged += new System.EventHandler(this.TxtSport_TextChanged);
             // 
             // BtnQuitter
             // 
-            this.BtnQuitter.Location = new System.Drawing.Point(536, 201);
+            this.BtnQuitter.Location = new System.Drawing.Point(699, 400);
             this.BtnQuitter.Name = "BtnQuitter";
             this.BtnQuitter.Size = new System.Drawing.Size(89, 38);
             this.BtnQuitter.TabIndex = 2;
             this.BtnQuitter.Text = "Quitter";
             this.BtnQuitter.UseVisualStyleBackColor = true;
+            this.BtnQuitter.Click += new System.EventHandler(this.BtnQuitter_Click);
             // 
             // Form1
             // 
@@ -96,6 +101,7 @@ namespace FrmSport
             this.GrpSaisieSport.ResumeLayout(false);
             this.GrpSaisieSport.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
--- FrmSport/Form1.cs
+++ FrmSport/Form1.cs
@@ -19,6 +19,52 @@ namespace FrmSport
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
+            
+        }
+
+        private void TxtSport_TextChanged(object sender, EventArgs e)
+        {
+            if(TxtSport.Text.Length >= 2)
+            {
+                BtnAjout.Enabled = true;
+            }
+            else
+            {
+                BtnAjout.Enabled = false;
+            }
+        }
+
+        private void BtnAjout_Click(object sender, EventArgs e)
+        {
+            // Création checkbox
+            CheckBox checkBox = new CheckBox();
+            checkBox.Text = this.TxtSport.Text;
+
+
+            // Ajout checkbox dans la liste
+            this.GrpSports.Controls.Add(checkBox);
+            int position = GrpSports.Controls.Count * 20;
+            checkBox.Location = new Point(checkBox.Location.X + 10, position);
+
+            
+            this.TxtSport.Text = "";
+        }
+
+        private void BtnQuitter_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Voulez vous vraiment quitter ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Bah pourquoi t'a cliquer sur la croix alors fils de pute");
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
